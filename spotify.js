06f242d7e6aafefe84e0b5b899aa32b6fb7d00bc
Dakota Bryant <bryant.dakota@gmail.com>
2017-04-20 11:29:42 -0500

    did first drill

---
+++ spotify.js
@@ -0,0 +1,18 @@
+var getFromApi = function(endpoint, query={}) {
+    const url = new URL(`https://api.spotify.com/v1/${endpoint}`);
+    Object.keys(query).forEach(key => url.searchParams.append(key, query[key]));
+    return fetch(url).then(function(response) {
+        if (!response.ok) {
+            return Promise.reject(response.statusText);
+        }
+        return response.json();
+    });
+};
+
+var artist;
+var getArtist = function(name) {
+    return getFromApi('search',{q: name, limit: 1, type: 'artist'})
+    .then(response => {
+      return response.artists.items[0];
+    });
+};
