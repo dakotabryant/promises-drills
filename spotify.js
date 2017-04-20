66e3ce9b6289635150f1014a2c084ab0f60d14cc
pham-thien <pham.thien@outlook.com>
2017-04-20 15:05:50 -0400

    add fetch related artists

--- spotify.js
+++ spotify.js
@@ -13,6 +13,12 @@ var artist;
 var getArtist = function(name) {
     return getFromApi('search',{q: name, limit: 1, type: 'artist'})
     .then(response => {
-      return response.artists.items[0];
+        console.log(response.artists.items[0].id);
+    //   return response.artists.items[0];
+      let id = response.artists.items[0].id;
+      console.log(`https://api.spotify.com/v1/artists/${id}/related-artists`);
+    //   return fetch(`https://api.spotify.com/v1/artists/${id}/related-artists`);
+    }).catch(err => {
+      console.error(err);
     });
 };
